11492da6fc28bb41ca17c7cd606e2c8dc819d50a
joaf <joaf.if@gmail.com>
2021-02-07 22:24:01 -0300

    Pirmeiro commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+
+<div class="p-col-6 p-offset-3" style="border:1px solid lightgray">
+  <div class="p-col-12 p-offset-1">
+    <h1>Instasuper #1</h1>
+    </div>
+  <div class="p-col-12">
+    <h1>Instasuper #2</h1>
+    <div class="p-grid">
+      <div class="profile-picture">
+        <img src="@/assets/blackWidow.png">
+      </div>
+      <div class="p-col-3">
+        <div class="p-d-flex p-flex-column p-mt-1 p-ml-2">
+          <div class="p-mb-2">
+            <b>Spiderman</b>
+          </div>
+          <div class="p-d-flex p-flex-column status">
+            <span class="p-mb-1"><b>74 </b>posts</span>
+            <span class="p-mb-1"><b>384 </b>curtidas</span>
+            <span><b>191 </b>comentários</span>
+          </div>
+        </div>
+      </div>
+      <div class="p-offset-1 p-col-6">
+        <div class=""><b> Visualizar como:</b></div>
+      <div class="p-d-flex p-grid">
+        <div class="p-mt-2 p-col-4 users">
+          <img src="@/assets/blackWidow.png" class="i">
+          <span >dsd</span>
+        </div>
+        <div class="p-mt-2 p-col-4 users">
+          <img src="@/assets/blackWidow.png"  class="i" >
+          <span >dsd</span>
+        </div>
+        <div class="p-mt-2 p-col-4 users">
+          <img src="@/assets/blackWidow.png" class="i">
+          <span >dsd</span>
+        </div>
+      </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+.profile-picture{
+  display: flex;
+  flex-direction: row;
+  width: 6em;
+  height: 6em;
+  overflow: hidden;
+  border-radius: 50%;
+}
+
+.profile-picture img {
+  display: inline;
+  margin: 0 auto;
+  height: 100%;
+  width: auto;
+}
+.status {
+  font-size: .85em;
+  font-weight: 500;
+  color: rgb(116, 116, 117);
+}
+.status b {
+ color:black
+}
+.t{
+  display: flex;
+  flex-direction: row;
+}
+.i{
+  border-radius: 50%;
+  height: 100%;
+  width: 42%;
+}
+.users{
+  border:1px solid rgb(39, 39, 39);
+  cursor:pointer;
+  width: 4em;
+  height: 4em;
+}
+.users:hover{
+  background-color: rgba(0, 0, 0, 0.123);
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+
+
+const app = createApp(App)
+
+app.use(PrimeVue);
+app.component('Card', Card);
+app.component('Button', Button);
+
+app.mount('#app')
